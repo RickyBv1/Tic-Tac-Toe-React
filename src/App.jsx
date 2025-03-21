0ecc3c705d3fa96238d95d796331e70db0a5eb77
RickyBv1 <rickycortes668668@gmail.com>
2025-03-21 14:28:01 -0400

    add game reset functionality and draw detection to Tic Tac Toe

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,16 @@ function App() {
     return null;
   };
 
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null);
+  }
+
   const updateBoard = (index) => {
     if (board[index] || winner) return;
 
@@ -62,17 +72,20 @@ function App() {
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
   };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset Game</button>
       <section className="game">
-        {board.map((_, index) => {
+        {board.map((square, index) => {
           return (
             <Square key={index} index={index} updateBoard={updateBoard}>
-              {board[index]}
+              {square}
             </Square>
           );
         })}
@@ -82,6 +95,21 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Draw!" : "Winner is:"}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <button onClick={resetGame}>Play Again</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
