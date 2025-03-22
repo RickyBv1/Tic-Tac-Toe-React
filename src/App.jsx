0e51cfef47d746b4fd9bb55eb75ff42bd6f9c050
RickyBv1 <rickycortes668668@gmail.com>
2025-03-21 20:26:57 -0400

    implement game state persistence with local storage and add reset functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,27 @@ import Square from "./components/Square";
 import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/Board";
 import WinnerModal from "./components/WinnerModal";
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/Index";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    resetGameStorage();
   };
 
   const updateBoard = (index) => {
@@ -27,6 +38,11 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn,
+    });
 
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
